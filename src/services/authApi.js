ecff0a10b4cdc1ea2f7b3b36bab1353badff1b55
starkweb108 <starkweb2@gmail.com>
2022-11-07 16:51:00 +0100

    lol

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -2,6 +2,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import axios from "axios";
 import { url } from "../utils.";
 
+axios.defaults.withCredentials = true;
 const axiosBaseQuery =
   ({ baseUrl } = { baseUrl: "" }) =>
   async ({ url, method, data, params }) => {
@@ -22,14 +23,13 @@ export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: axiosBaseQuery({
     baseUrl: `${url}/api/v1/auth`,
-    withCredentials: true,
   }),
   endpoints: (build) => ({
     loginAdmin: build.mutation({
       query: (body) => {
         return {
           url: "/login",
-          method: "POST",
+          method: "post",
           data: body,
         };
       },
@@ -38,7 +38,7 @@ export const authApi = createApi({
       query: (body) => {
         return {
           url: "/logout",
-          method: "DELETE",
+          method: "delete",
           data: body,
         };
       },
@@ -47,7 +47,7 @@ export const authApi = createApi({
       query: (body) => {
         return {
           url: "/show-admin",
-          method: "POST",
+          method: "post",
           data: body,
         };
       },
