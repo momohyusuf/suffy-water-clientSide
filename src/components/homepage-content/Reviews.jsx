d55f4e8e9afc946e57289dead98a8e53cff3395e
starkweb108 <starkweb2@gmail.com>
2022-11-25 10:22:06 +0100

    made some redesigning

--- src/components/homepage-content/Reviews.jsx
+++ src/components/homepage-content/Reviews.jsx
@@ -1,100 +1,100 @@
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { MdLocationPin } from "react-icons/md";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { MdLocationPin } from 'react-icons/md';
 
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
-import "swiper/scss";
-import "swiper/scss/navigation";
-import "swiper/scss/pagination";
-import "swiper/scss/scrollbar";
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
 const Reviews = () => {
   return (
     <section className="reviews--container">
-      <div className="slider--container">
-        <Swiper
-          // install Swiper modules
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={100}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          <SwiperSlide>
-            <div>
-              {" "}
-              <h3>Muhammed Abubakar</h3>
-              <small>
-                <MdLocationPin color="#D62246" />
-                Auchi, Edo State.
-              </small>
-              <p>
-                <q>
-                  I have been a customer of suffy's water for 5years+. They've
-                  never disappointed me once.
-                </q>
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              {" "}
-              <h3>Rejoice Sammuel</h3>
-              <small>
-                <MdLocationPin color="#D62246" />
-                Aviele, Edo State.
-              </small>{" "}
-              <p>
-                <q>
-                  My Customers dey always ask for one water and na suffy water.
-                  they say e clean, no get smell, no get taste and e good for
-                  their body
-                </q>
-              </p>
-            </div>
-          </SwiperSlide>
+      <div className="reviews--content">
+        <h2>REVIEWS</h2>
+        <div className="slider--container">
+          <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={100}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            <SwiperSlide>
+              <div>
+                {' '}
+                <h3>Muhammed Abubakar</h3>
+                <small>
+                  <MdLocationPin color="#D62246" />
+                  Auchi, Edo State.
+                </small>
+                <p>
+                  <q>
+                    I have been a customer of suffy's water for 5years+. They've
+                    never disappointed me once.
+                  </q>
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div>
+                {' '}
+                <h3>Rejoice Sammuel</h3>
+                <small>
+                  <MdLocationPin color="#D62246" />
+                  Aviele, Edo State.
+                </small>{' '}
+                <p>
+                  <q>
+                    My Customers dey always ask for one water and na suffy
+                    water. they say e clean, no get smell, no get taste and e
+                    good for their body
+                  </q>
+                </p>
+              </div>
+            </SwiperSlide>
 
-          <SwiperSlide>
-            <div>
-              {" "}
-              <h3>Aminat Isah</h3>
-              <small>
-                <MdLocationPin color="#D62246" />
-                Agbede, Edo State.
-              </small>
-              <p>
-                {" "}
-                <q>
-                  Since the introduction of their online service, i worry less
-                  about getting water now. I just place order online and wait
-                  for them to deliver to me. And they are always fast
-                </q>
-              </p>
-            </div>
-          </SwiperSlide>
+            <SwiperSlide>
+              <div>
+                {' '}
+                <h3>Aminat Isah</h3>
+                <small>
+                  <MdLocationPin color="#D62246" />
+                  Agbede, Edo State.
+                </small>
+                <p>
+                  {' '}
+                  <q>
+                    Since the introduction of their online service, i worry less
+                    about getting water now. I just place order online and wait
+                    for them to deliver to me. And they are always fast
+                  </q>
+                </p>
+              </div>
+            </SwiperSlide>
 
-          <SwiperSlide>
-            <div>
-              {" "}
-              <h3>Maimunat Oshoke</h3>
-              <small>
-                <MdLocationPin color="#D62246" />
-                Auchi, Edo State.
-              </small>
-              <p>
-                {" "}
-                <q>
-                  Their drivers are among they best. they have good customer
-                  relationship
-                </q>
-              </p>
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div>
-        <h2>What they are saying about us</h2>
+            <SwiperSlide>
+              <div>
+                {' '}
+                <h3>Maimunat Oshoke</h3>
+                <small>
+                  <MdLocationPin color="#D62246" />
+                  Auchi, Edo State.
+                </small>
+                <p>
+                  {' '}
+                  <q>
+                    Their drivers are among they best. they have good customer
+                    relationship
+                  </q>
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </section>
   );
--- src/components/homepage-content/WhyUs.jsx
+++ src/components/homepage-content/WhyUs.jsx
@@ -1,19 +1,35 @@
-import React from "react";
-import Slider from "./Slider";
+import React from 'react';
 
 const WhyUs = () => {
   return (
     <section className="why--us--container">
-      <div
-        style={{
-          width: "700px",
-          maxWidth: "100%",
-        }}
-      >
-        <h2>Why you should trust us with your drinking water</h2>
+      <div>
+        <h2>Modern Technology</h2>
+        <p>
+          Using state of the art facilities, we ensure that our water go through
+          rigorous and proper treatment process using industry guidelines
+          stipulated by NAFDAC and also ensuring it is safe for human
+          consumption.
+        </p>
       </div>
-      <div className="slider--container">
-        <Slider />
+      <div>
+        <div>
+          {' '}
+          <h2>Over 8years Experience</h2>
+          <p>
+            We've been providing affordable drinking Water to Nigerians for over
+            8 years.{' '}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <h2>Customer's Satisfaction</h2>
+        <p>
+          Trusted by more than 100,000 household. Over the years we've made
+          customer's satisfaction our priority. We're continually exploring ways
+          to even satisfy our customers better.
+        </p>
       </div>
     </section>
   );
