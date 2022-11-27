140b4632405f5745c18fdd51bbf8abf5cda1c5cc
starkweb108 <starkweb2@gmail.com>
2022-11-27 12:40:13 +0100

    updated modal styling

--- src/components/homepage-content/ImageIllustrations.jsx
+++ src/components/homepage-content/ImageIllustrations.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 const ImageIllustrations = () => {
   return (
     <section className="image--illustration--container">
       <div className="image--illustration--content">
-        <div className="image--employees">
-          <p>Over 50 Employees</p>
-        </div>
-
         <div className="image--facilities">
           <p>State of the art facilities</p>
         </div>
 
         <div className="image--delivery">
           <p>Fast delivery </p>
+        </div>
+
+        <div className="image--environment">
+          <p>Clean environment</p>
         </div>
       </div>
     </section>
