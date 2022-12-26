2bd0e55f3781c3ebaea9603c67836ad8415fd2eb
starkweb108 <starkweb2@gmail.com>
2022-12-26 22:59:09 +0100

    made some styling changes

--- src/components/homepage-content/Reviews.jsx
+++ src/components/homepage-content/Reviews.jsx
@@ -1,5 +1,7 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { MdLocationPin } from 'react-icons/md';
+import { AiFillStar } from 'react-icons/ai';
+import { reviews } from '../../data/reviews';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
@@ -23,76 +25,32 @@ const Reviews = () => {
             onSwiper={(swiper) => console.log(swiper)}
             onSlideChange={() => console.log('slide change')}
           >
-            <SwiperSlide>
-              <div>
-                {' '}
-                <h3>Muhammed Abubakar</h3>
-                <small>
-                  <MdLocationPin color="#D62246" />
-                  Auchi, Edo State.
-                </small>
-                <p>
-                  <q>
-                    I have been a customer of suffy's water for 5years+. They've
-                    never disappointed me once.
-                  </q>
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div>
-                {' '}
-                <h3>Rejoice Sammuel</h3>
-                <small>
-                  <MdLocationPin color="#D62246" />
-                  Aviele, Edo State.
-                </small>{' '}
-                <p>
-                  <q>
-                    My Customers dey always ask for one water and na suffy
-                    water. they say e clean, no get smell, no get taste and e
-                    good for their body
-                  </q>
-                </p>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div>
-                {' '}
-                <h3>Aminat Isah</h3>
-                <small>
-                  <MdLocationPin color="#D62246" />
-                  Agbede, Edo State.
-                </small>
-                <p>
-                  {' '}
-                  <q>
-                    Since the introduction of their online service, i worry less
-                    about getting water now. I just place order online and wait
-                    for them to deliver to me. And they are always fast
-                  </q>
-                </p>
-              </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <div>
-                {' '}
-                <h3>Maimunat Oshoke</h3>
-                <small>
-                  <MdLocationPin color="#D62246" />
-                  Auchi, Edo State.
-                </small>
-                <p>
-                  {' '}
-                  <q>
-                    Their drivers are among they best. they have good customer
-                    relationship
-                  </q>
-                </p>
-              </div>
-            </SwiperSlide>
+            {reviews.map((review, index) => (
+              <SwiperSlide key={index}>
+                <div>
+                  <h3>{review.name}</h3>
+                  <small
+                    style={{
+                      margin: '0.4rem 0',
+                    }}
+                  >
+                    <MdLocationPin color="#D62246" />
+                    {review.location}
+                  </small>
+                  <div>
+                    <AiFillStar color="#FED700" />
+                    <AiFillStar color="#FED700" />
+                    <AiFillStar color="#FED700" />
+                    <AiFillStar color="#FED700" />
+                    <AiFillStar color="#FED700" />
+                  </div>
+                  <p>
+                    {' '}
+                    <q>{review.text}</q>
+                  </p>{' '}
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
---
+++ src/data/reviews.js
@@ -0,0 +1,26 @@
+export const reviews = [
+  {
+    id: 1,
+    name: 'Muhammed Abubakar',
+    location: 'Auchi, Edo State',
+    text: "I have been a customer of suffy's water for 5years+. They've never disappointed me once.",
+  },
+  {
+    id: 2,
+    name: 'Rejoice Sammuel',
+    location: 'Aviele, Edo State.',
+    text: ' My Customers dey always ask for one water and na suffy water. they say e clean, no get smell, no get taste and e good for their body',
+  },
+  {
+    id: 3,
+    name: 'Aminat Isah',
+    location: ' Agbede, Edo State.',
+    text: ' Since the introduction of their online service, i worry less about getting water now. I just place order online and wait for them to deliver to me. And they are always fast',
+  },
+  {
+    id: 5,
+    name: 'Maimunat Oshoke',
+    location: 'Auchi, Edo State',
+    text: ' Their drivers are among they best. they have good customer relationship',
+  },
+];
