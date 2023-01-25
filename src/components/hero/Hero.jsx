6dfa9cf25417ec7391c0dcb3d59a7bd010383375
starkweb108 <starkweb2@gmail.com>
2023-01-25 13:11:47 +0100

    updating the web pages to use proper html

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,20 +7,22 @@ const Hero = () => {
   return (
     <section className="hero--container">
       <div className="hero--items">
-        <article className="brand--description">
+        <div className="brand--description">
           <h1>Don't just drink water, Drink clean and healthy water</h1>
           <p>
             Providing Nigerians with easy access to clean, affordable, and
             healthy drinking water
           </p>
           <CustomBtn />
-        </article>
+        </div>
         <div>
-          <img
-            src={drinkWater}
-            alt="drinking-water"
-            className="drink-water-image"
-          />
+          <picture>
+            <img
+              src={drinkWater}
+              alt="drinking-water"
+              className="drink-water-image"
+            />
+          </picture>
         </div>
       </div>
     </section>
